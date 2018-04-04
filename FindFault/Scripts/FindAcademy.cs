1cf1f07272c542690b4558bce6a1d317ab8c90e5
ckhoon <ckhoon@tp.edu.sg>
2018-04-04 18:34:00 +0800

    first add

---
+++ FindFault/Scripts/FindAcademy.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindAcademy : Academy {
+
+    public int CorrectA;
+    public int CorrectB;
+    public int Wrong;
+    public GameObject CorrectPreFab;
+    public GameObject CorrectParentA;
+    public GameObject CorrectParentB;
+    public GameObject WrongPreFab;
+    public GameObject WrongParent;
+    public GameObject WallTop;
+    public GameObject WallBottom;
+    public GameObject WallLeft;
+    public GameObject WallRight;
+
+    private static int ALL_COLOR = 5;
+    private Color[] testColor = new Color[ALL_COLOR];
+
+
+    public override void InitializeAcademy()
+    {
+        testColor[0] = Color.blue;
+        testColor[1] = Color.gray;
+        testColor[2] = Color.magenta;
+        testColor[3] = Color.red;
+        testColor[4] = Color.green;
+
+        //Correct = 2;
+        //Wrong = 1;
+    }
+
+
+    public override void AcademyReset()
+    {
+        CorrectA = Random.Range(0, 4);
+        CorrectB = Random.Range(1, 4);
+        Wrong = Random.Range(1, 4);
+
+        //Debug.Log("Academy reset");
+        for (int i = CorrectParentA.transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(CorrectParentA.transform.GetChild(i).gameObject);
+        }
+        for (int i = CorrectParentB.transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(CorrectParentB.transform.GetChild(i).gameObject);
+        }
+        for (int i = WrongParent.transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(WrongParent.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < CorrectA; i++)
+        {
+            float x = Random.Range(CorrectParentA.transform.position.x - CorrectParentA.GetComponent<Renderer>().bounds.size.x/2 + CorrectPreFab.GetComponent<Renderer>().bounds.size.x / 2,
+                                    CorrectParentA.transform.position.x + CorrectParentA.GetComponent<Renderer>().bounds.size.x / 2 - CorrectPreFab.GetComponent<Renderer>().bounds.size.x / 2);
+            float y = Random.Range(CorrectParentA.transform.position.z - CorrectParentA.GetComponent<Renderer>().bounds.size.z / 2 + CorrectPreFab.GetComponent<Renderer>().bounds.size.z / 2,
+                                    CorrectParentA.transform.position.z + CorrectParentA.GetComponent<Renderer>().bounds.size.z / 2 - CorrectPreFab.GetComponent<Renderer>().bounds.size.z / 2);
+
+            GameObject objCorrect = Instantiate(
+               CorrectPreFab,
+               new Vector3(x, 0.1f, y),
+               Quaternion.identity);
+               
+            objCorrect.transform.parent = CorrectParentA.transform;
+            objCorrect.GetComponent<Renderer>().material.color = testColor[i % ALL_COLOR];
+            objCorrect.tag = "correct";
+        }
+
+        for (int i = 0; i < CorrectB; i++)
+        {
+            float x = Random.Range(CorrectParentB.transform.position.x - CorrectParentB.GetComponent<Renderer>().bounds.size.x / 2 + CorrectPreFab.GetComponent<Renderer>().bounds.size.x / 2,
+                                    CorrectParentB.transform.position.x + CorrectParentB.GetComponent<Renderer>().bounds.size.x / 2 - CorrectPreFab.GetComponent<Renderer>().bounds.size.x / 2);
+            float y = Random.Range(CorrectParentB.transform.position.z - CorrectParentB.GetComponent<Renderer>().bounds.size.z / 2 + CorrectPreFab.GetComponent<Renderer>().bounds.size.z / 2,
+                                    CorrectParentB.transform.position.z + CorrectParentB.GetComponent<Renderer>().bounds.size.z / 2 - CorrectPreFab.GetComponent<Renderer>().bounds.size.z / 2);
+
+            GameObject objCorrect = Instantiate(
+               CorrectPreFab,
+               new Vector3(x, 0.1f, y),
+               Quaternion.identity);
+
+            objCorrect.transform.parent = CorrectParentB.transform;
+            objCorrect.GetComponent<Renderer>().material.color = testColor[i % ALL_COLOR];
+            objCorrect.tag = "correct";
+        }
+
+        for (int i = 0; i < Wrong; i++)
+        {
+            float x = Random.Range(WrongParent.transform.position.x - WrongParent.GetComponent<Renderer>().bounds.size.x / 2 + WrongPreFab.GetComponent<Renderer>().bounds.size.x / 2,
+                                    WrongParent.transform.position.x + WrongParent.GetComponent<Renderer>().bounds.size.x / 2 - WrongPreFab.GetComponent<Renderer>().bounds.size.x / 2);
+            float y = Random.Range(WrongParent.transform.position.z - WrongParent.GetComponent<Renderer>().bounds.size.z / 2 + WrongPreFab.GetComponent<Renderer>().bounds.size.z / 2,
+                                    WrongParent.transform.position.z + WrongParent.GetComponent<Renderer>().bounds.size.z / 2 - WrongPreFab.GetComponent<Renderer>().bounds.size.z / 2);
+            GameObject objCorrect = Instantiate(
+               WrongPreFab,
+               new Vector3(x, 0.1f, y),
+               Quaternion.identity);
+            objCorrect.transform.parent = WrongParent.transform;
+            objCorrect.tag = "wrong";
+        }
+    }
+
+    public override void AcademyStep()
+    {
+    }
+}
---
+++ FindFault/Scripts/FindAgentCamera.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindAgentCamera : Agent
+{
+    private FindAcademy academy;
+
+    public GameObject CorrectCarArea;
+    public GameObject WrongCarArea;
+    public Text txtReward;
+
+    private float distance;
+    private GameObject CorrectCar;
+    //private GameObject WrongCar;
+    private static float WRONG_CAR_DIST = 0.2f;
+
+    public override void InitializeAgent()
+    {
+        academy = FindObjectOfType(typeof(FindAcademy)) as FindAcademy;
+        academy.AcademyReset();
+        getNearestCar();
+
+        distance = Vector3.Distance(transform.position, CorrectCar.transform.position);
+    }
+
+    public override void CollectObservations()
+    {
+        GameObject[] wrongCars = GameObject.FindGameObjectsWithTag("wrong");
+        for (int i = 0; i < 3; i++)
+        {
+            if (i < wrongCars.Count())
+            {
+                AddVectorObs(wrongCars[i].transform.position.x);
+                AddVectorObs(wrongCars[i].transform.position.z);
+            }
+            else
+            {
+                AddVectorObs(99);
+                AddVectorObs(99);
+            }
+        }
+        AddVectorObs(CorrectCar.transform.position.x);
+        AddVectorObs(CorrectCar.transform.position.z);
+        AddVectorObs(transform.position.x);
+        AddVectorObs(transform.position.z);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        txtReward.text = GetReward().ToString();
+        int action = Mathf.FloorToInt(vectorAction[0]);
+
+        Vector3 targetPos = transform.position;
+        if (action == 0)
+        {
+            targetPos = transform.position + new Vector3(.01f, 0, 0f);
+        }
+
+        if (action == 1)
+        {
+            targetPos = transform.position + new Vector3(-.01f, 0, 0f);
+        }
+
+        if (action == 2)
+        {
+            targetPos = transform.position + new Vector3(0f, 0, .01f);
+        }
+
+        if (action == 3)
+        {
+            targetPos = transform.position + new Vector3(0f, 0, -.01f);
+        }
+
+        Collider[] blockTest = Physics.OverlapSphere(targetPos, GetComponent<SphereCollider>().radius * .2f);
+        if (blockTest.Where(col => col.gameObject.tag == "correct").ToArray().Length == 1)
+        {
+            SetReward(.8f);
+            txtReward.text = GetReward().ToString();
+            //Debug.Log(blockTest.Where(col => col.gameObject.tag == "correct").ToArray()[0]);
+            DestroyImmediate(blockTest.Where(col => col.gameObject.tag == "correct").ToArray()[0].gameObject);
+            if (!getNearestCar())
+            {
+                Done();
+                return;         
+            }
+        }
+        else if (blockTest.Where(col => col.gameObject.tag == "wrong").ToArray().Length == 1)
+        {
+            SetReward(-1f);
+            txtReward.text = GetReward().ToString();
+            Done();
+            return;
+        }
+
+        transform.position = targetPos;
+        if (Vector3.Distance(targetPos, CorrectCar.transform.position) >= distance)
+        {
+            //AddReward(-0.01f);
+        }
+        else
+        {
+            AddReward(0.01f);
+            distance = Vector3.Distance(targetPos, CorrectCar.transform.position);
+        }
+        //distance = Vector3.Distance(targetPos, CorrectCar.transform.position);
+
+        GameObject[] wrongCars = GameObject.FindGameObjectsWithTag("wrong");
+        foreach (GameObject wrongCar in wrongCars)
+        {
+            if (Vector3.Distance(targetPos, wrongCar.transform.position) <= WRONG_CAR_DIST)
+            {
+                //Debug.Log("too near");
+                AddReward(-0.1f);
+            }
+        }
+        //Done();
+    }
+
+    public override void AgentReset()
+    {
+        transform.position = new Vector3(0f, 0.15f, 0f);
+        academy.AcademyReset();
+        getNearestCar();
+        distance = Vector3.Distance(transform.position, CorrectCar.transform.position);
+    }
+
+    private bool getNearestCar()
+    {
+        float shortest = 999;
+        CorrectCar = null;
+        /*
+        foreach (Transform child in CorrectCarArea.transform)
+        {
+            if (child.tag == "correct")
+            {
+                if (Vector3.Distance(child.position, transform.position) < shortest)
+                {
+                    CorrectCar = child.gameObject;
+                    shortest = Vector3.Distance(child.position, transform.position);
+                }
+            }
+        }
+        */
+        GameObject[] correctCars = GameObject.FindGameObjectsWithTag("correct");
+        //Debug.Log("cars - " + correctCars.Count());
+        foreach (GameObject child in correctCars)
+        {
+            //Debug.Log("distance = " +  Vector3.Distance(child.transform.position, transform.position));
+            if (Vector3.Distance(child.transform.position, transform.position) < shortest)
+            {
+                CorrectCar = child;
+                shortest = Vector3.Distance(child.transform.position, transform.position);
+            }
+        }
+        
+        /*
+        foreach (Transform child in WrongCarArea.transform)
+        {
+            if (child.tag == "wrong")
+                WrongCar = child.gameObject;
+        }
+        */
+
+        if (CorrectCar == null)
+        {
+            //Debug.Log("No more car");
+            return false;
+        }
+        else
+        {
+            CorrectCar.GetComponent<Renderer>().material.color = Color.black;
+            distance = Vector3.Distance(transform.position, CorrectCar.transform.position);
+            return true;
+        }
+    }
+
+}
